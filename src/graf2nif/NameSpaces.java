c4d800b24525f3ddc36adde69f394d66a477d910
bsimon2 <bsimon2@users.noreply.github.com>
2015-03-08 19:59:26 +0100

    add class containing namespaces

---
+++ src/graf2nif/NameSpaces.java
@@ -0,0 +1,14 @@
+package graf2nif;
+
+public class NameSpaces {
+	
+	public static final String nifCoreNS = "http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#";
+	public static final String lider = "data.lider-project.eu/";
+	public static final String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
+	public static final String lexinfo = "http://www.lexinfo.net/ontology/2.0/lexinfo#";
+	public static final String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	public static final String xml = "http://www.w3.org/XML/1998/namespace/";
+	public static final String xsi = "http://www.w3.org/2001/XMLSchema-instance#";
+	public static final String xsd = "http://www.w3.org/2001/XMLSchema#";
+	
+}
